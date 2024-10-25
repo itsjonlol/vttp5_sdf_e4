0daa3bcfe40ef89af934b84e71fbb1df2e8aabc2
itsjonlol <jonathansuherman@gmail.com>
2024-10-25 16:31:01 +0800

    deleted some comments

--- GolZipReader.java
+++ GolZipReader.java
@@ -8,22 +8,7 @@ import java.util.zip.ZipInputStream;
 public class GolZipReader {
     public static void main(String[] args) {
         String zipFilePath = "gol_2023_11_09-07_19_51.zip"; // Update with your zip file path
-        
-        // try (ZipInputStream zipStream = new ZipInputStream(new FileInputStream(zipFilePath))) {
-        //     ZipEntry entry;
-            
-        //     // Iterate through each entry in the .zip file
-        //     while ((entry = zipStream.getNextEntry()) != null) {
-        //         // Check if the entry is a .gol file
-        //         if (entry.getName().endsWith(".gol")) {
-        //             System.out.println("Reading file: " + entry.getName());
-        //             readGolFile(zipStream);
-        //         }
-        //         zipStream.closeEntry(); // Close the current entry to move to the next
-        //     }
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+    
 
         try (FileInputStream is = new FileInputStream(zipFilePath)) {
             ZipInputStream zipStream = new ZipInputStream(is);
